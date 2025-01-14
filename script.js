6221396c1084ebc9bd7159ae46efd083dc727e40
Sb-CCL <sbcolorcodedlabs@gmail.com>
2025-01-14 14:26:30 -0500

    created an array for questions in script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello Words")
+const flashcards = [
+  {
+    question: "What are Parameters and Arguments?",
+    answer:
+      "Parameters are variables listed in the function definition. Arguments are actual values passed to the function when it is called.",
+  },
+];
