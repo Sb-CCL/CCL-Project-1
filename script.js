5517aeec2912389c9baa685047bc3d2e5db6f248
Sb-CCL <sbcolorcodedlabs@gmail.com>
2025-01-14 19:06:19 -0500

    added code to script.js

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ const flashcards = [
     answer:
       "Parameters are variables listed in the function definition. Arguments are actual values passed to the function when it is called.",
   },
+  {
+    question: "What is JavaScript?",
+    answer: "JavaScript is a programming language used for web developement.",
+  },
 ];
+
+// For loop that loops through the flashcards objects
+for (let i = 0; i < flashcards.length; i++) {
+  // Question & Answers
+  //   console.log(flashcards[i]);
+  // Questions
+  //   console.log(`Question: ${flashcards[i].question}`);
+  // Answers
+  //   console.log(`Answer: ${flashcards[i].answer}`);
+}
+
+// Event Listener - Next Question Button
+const nextQuestionButton = document.getElementById("nextQuestionButton");
+nextQuestionButton.addEventListener("click", function () {
+  console.log(`Question: ${flashcards[i].question}`);
+});
+
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+
+// question.append("Test");
+
+// console.log(question);
+// console.log(answer);
