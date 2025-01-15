ac6843fda77cdfefb228fb116185cdab4c8e5946
Sb-CCL <sbcolorcodedlabs@gmail.com>
2025-01-14 19:58:34 -0500

    added code to script.js

--- script.js
+++ script.js
@@ -13,28 +13,24 @@ const flashcards = [
 let arrQuestions = 0;
 
 // For loop that loops through the flashcards objects
-for (let i = 0; i < flashcards.length; i++) {
-  // Question & Answers
-  //   console.log(flashcards[i]);
-  // Questions
-  //   console.log(`Question: ${flashcards[i].question}`);
-  // Answers
-  //   console.log(`Answer: ${flashcards[i].answer}`);
-}
+// for (let i = 0; i < flashcards.length; i++) {
+// Question & Answers
+//   console.log(flashcards[i]);
+// Questions
+//   console.log(`Question: ${flashcards[i].question}`);
+// Answers
+//   console.log(`Answer: ${flashcards[i].answer}`);
+// }
 
-// Event Listener - Answer Button
-const answerButton = document.getElementById("answerButton");
-let answerQuestion = 0;
-answerButton.addEventListener("click", function () {
-  console.log(`Answer: ${flashcards[answerQuestion].answer}`);
-  nextQuestion++;
-});
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
 
 // Event Listener - Next Question Button
 const nextQuestionButton = document.getElementById("nextQuestionButton");
 let nextQuestion = 0;
 nextQuestionButton.addEventListener("click", function () {
-  console.log(`Question: ${flashcards[nextQuestion].question}`);
+  console.log(flashcards[nextQuestion].question);
+  console.log(flashcards[nextQuestion].answer);
   nextQuestion++;
 });
 
@@ -42,12 +38,23 @@ nextQuestionButton.addEventListener("click", function () {
 const prevQuestionButton = document.getElementById("prevQuestionButton");
 let prevQuestion = 0;
 prevQuestionButton.addEventListener("click", function () {
-  console.log(`Question: ${flashcards[prevQuestion].question}`);
+  console.log(flashcards[prevQuestion].question);
+  console.log(flashcards[prevQuestion].answer);
   prevQuestion--;
 });
 
-const question = document.getElementById("question");
-const answer = document.getElementById("answer");
+const questionElement = document.getElementById("question");
+
+// Event Listener - Answer Button
+const answerButton = document.getElementById("answerButton");
+let answerQuestion = 0;
+
+answerButton.addEventListener("click", function () {
+  console.log(flashcards[answerQuestion].question);
+  console.log(flashcards[answerQuestion].answer);
+  questionElement.textContent = flashcards[answerQuestion].question;
+  answerQuestion++;
+});
 
 // question.append("Test");
 
